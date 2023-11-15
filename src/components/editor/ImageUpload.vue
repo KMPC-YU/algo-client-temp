ce08d73298466b1143810fca3bf25fc67272a3e8
MINHYUK <shiro_hacker@kakao.com>
2023-11-16 04:46:54 +0900

    feat / 게시글 이미지 추가 API 연결

--- src/components/editor/ImageUpload.vue
+++ src/components/editor/ImageUpload.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <input type="file" ref="fileInput" @change="handleFileUpload" class="d-none" />
+    <input type="file" ref="oldFile" @change="handleFileUpload" class="d-none" />
   </div>
 <!--  <div class="modal" v-if="show">-->
 <!--    <div class="modal-dialog">-->
@@ -27,21 +27,135 @@
 
 <script>
 import { ref } from 'vue'
+import axios from 'axios'
 
 export default {
   setup(props, context) {
-    const fileInput = ref(null);
+    const imageSrc = ref('')
+    const oldFile = ref(null)
+    const newFile = ref(null)
+    const fileName = ref('')
+    let imgUrl
     const openFileInput = () => {
-      fileInput.value.click();
+      oldFile.value.click();
     }
 
     const handleFileUpload = () => {
-      const file = fileInput.value.files[0];
-      console.log(file);
+      const blobURL = URL.createObjectURL(oldFile.value.files[0]);
+      const img = new Image();
+      img.src = blobURL;
+      img.onload = function () {
+        imageSizeChange(img,oldFile.value.files[0].size)
+        let list = new DataTransfer();
+        list.items.add(new File([dataURItoBlob(imgUrl)], oldFile.value.files[0].name,{
+          type: oldFile.value.files[0].type,
+        }));
+        newFile.value = list.files
+      }
+      fileName.value = oldFile.value.files[0].name
+      requestAuth()
+    }
+
+    function dataURItoBlob(dataURI) {
+      let byteString = atob(dataURI.split(',')[1]);
+      let mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+      let ab = new ArrayBuffer(byteString.length);
+      let ia = new Uint8Array(ab);
+      for (let i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+      }
+      return new Blob([ab], {type: mimeString});
+    }
+
+    const imageSizeChange = (image,size) => {
+      let canvas = document.createElement("canvas"),
+          max_size = 600,
+          width = image.width,
+          height = image.height;
+      if (width > height) {
+        // 가로가 길 경우
+        if (width > max_size) {
+          height *= max_size / width;
+          width = max_size;
+        }
+      } else {
+        // 세로가 길 경우
+        if (height > max_size) {
+          width *= max_size / height;
+          height = max_size;
+        }
+      }
+      canvas.width = width;
+      canvas.height = height;
+      canvas.getContext("2d").drawImage(image, 0, 0, width, height);
+      if(size>3*1024*1024){
+        imgUrl = canvas.toDataURL("image/jpeg", 0.5);
+      }
+      else{
+        imgUrl = canvas.toDataURL("image/jpeg", 1);
+      }
+    }
+
+    const filePath = ref('')
+
+    const requestAuth = () => { // 이미지 업로드 권한 취득
+      console.log('RequestAuth Called')
+      axios.post('http://be.algo.r-e.kr:8080/api/v1/posts/images', {
+        file_name: fileName.value,
+        image_request_type: "POST",
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+        },
+        withCredentials: true,
+      }).then((res) => {
+        filePath.value = res.data.substring(res.data.indexOf('amazonaws.com') + 14, res.data.indexOf('?'))  // 이미지이름.확장자
+        console.log(filePath.value)
+        uploadImage(res.data)
+      }).catch ((err) => {
+        console.error(err)
+      })
+    }
+
+    const uploadImage = (awsURL) => { // 백엔드 서버에 URL 요청
+      axios.put(awsURL, newFile.value[0], {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          'Access-Control-Allow-Origin': '*',
+        },
+        withCredentials: true,
+      }).then((res) => {
+        insertImage(filePath.value)
+      }).catch((err) => {
+        console.error(err)
+      })
+    }
+
+    const insertImage = (imageUrl) => { // 이미지 글쓰기 본문에 삽입
+      axios.post('http://be.algo.r-e.kr:8080/api/v1/posts/images', {
+        file_name: imageUrl,  // 이미지이름.확장자
+        image_request_type: "GET",
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+        },
+        withCredentials: true,
+      }).then ((res) => {
+        console.log(res.data)
+        imageSrc.value = res.data.substring(0, res.data.indexOf('?'))
+        context.emit('onConfirm', {
+          src: imageSrc.value
+        })
+        closeModal()
+      }).catch ((err) => {
+        console.error(err)
+      })
     }
 
     return {
-      fileInput,
+      oldFile,
       openFileInput,
       handleFileUpload,
     }
